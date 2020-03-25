98ee770beaea85dcb9ca7b1d27606ab17908caeb
Gabriel Prevelate <gabriel.prevelate@nova-8.com>
2020-03-25 10:36:47 -0300

    just a test2

--- Android/MSTG-Android-Kotlin-App/app/src/test/java/sg/vantagepoint/mstgkotlin/ExampleUnitTest.kt
+++ Android/MSTG-Android-Kotlin-App/app/src/test/java/sg/vantagepoint/mstgkotlin/ExampleUnitTest.kt
@@ -7,7 +7,8 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing). just test 4
+ * See [testing documentation](http://d.android.com/tools/testing).
+ * CxFlow with WebHook.
  */
 class ExampleUnitTest {
     @Test
